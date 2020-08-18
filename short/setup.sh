f03a96a1ab1c56cc34290da1ce77af4406f20210
SyrakuShaikh(Hao-Kai SUN) <spin.hk@outlook.com>
2020-08-18 14:21:59 +0800

    remove unused libssl.

--- short/setup.sh
+++ short/setup.sh
@@ -1,5 +1,4 @@
 SHORTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scratchfs/bes/sunhk/6.6.4.p01/libssl/usr/lib
 alias Hepsub='python2 ${SHORTDIR}/hepsubnew.py'
 alias Heprm='python2 ${SHORTDIR}/Heprm.py'
 alias Hep_q='python2 ${SHORTDIR}/Hep_q.py'

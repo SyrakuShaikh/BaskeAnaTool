0516695974cfa68beed146e14a1fb26e958ec8fb
SyrakuShaikh(Hao-Kai SUN) <spin.hk@outlook.com>
2020-12-01 16:50:22 +0800

    add new SimAndRec template for DIY705Jpsi.

--- SimAndRec/addSimor.py
+++ SimAndRec/addSimor.py
@@ -4,16 +4,18 @@ import os
 
 
 def addSimor():
-    name = ["Jpsi", "NewJpsi", "Psi2S", "4180", "705Jpsi", "Diy4180"]
+    name = ["Jpsi", "NewJpsi", "Psi2S", "4180", "705Jpsi", "Diy4180", "DIY705Jpsi"]
     simff = [
         "template/simJpsi.txt", "template/simNewJpsi.txt",
         "template/simPsi2S.txt", "template/sim4180.txt",
-        "template/sim705Jpsi.txt", "template/simDiy4180.txt"
+        "template/sim705Jpsi.txt", "template/simDiy4180.txt",
+        "template/simDIY705Jpsi.txt"
     ]
     recff = [
         "template/recJpsi.txt", "template/recNewJpsi.txt",
         "template/recPsi2S.txt", "template/rec4180.txt",
-        "template/rec705Jpsi.txt", "template/recDiy4180.txt"
+        "template/rec705Jpsi.txt", "template/recDiy4180.txt",
+        "template/recDIY705Jpsi.txt"
     ]
 
     for n, s, r in zip(name, simff, recff):
